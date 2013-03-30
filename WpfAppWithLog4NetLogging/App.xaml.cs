4634c869732c2bcabdbcbb9f087ca34dfcdc4ba7
Christian Pfahl <cp@krinito.de>
2013-03-30 02:46:42 +0100

    Arbeitsstand; Grid Binding klappt nicht korrekt

--- WpfAppWithLog4NetLogging/App.xaml.cs
+++ WpfAppWithLog4NetLogging/App.xaml.cs
@@ -1,16 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
+﻿using System.Configuration;
+using System.Data.SqlServerCe;
+using System.Reflection;
 using System.Windows;
+
+using log4net;
+using log4net.Config;
+[assembly : XmlConfigurator]
+
 
 namespace Log4NetReusesConnectionStringDemo
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog log_ = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        public App()
+        {
+            log_.Debug("Hello World!");
+        }
     }
 }
--- WpfAppWithLog4NetLogging/MainWindow.xaml.cs
+++ WpfAppWithLog4NetLogging/MainWindow.xaml.cs
@@ -1,27 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlServerCe;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Linq;
+
+using log4net;
+
 
 namespace Log4NetReusesConnectionStringDemo
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly ILog log_ = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly SqlCeConnection sqlConn_ =
+                new SqlCeConnection(ConfigurationManager.ConnectionStrings["LocalDatabase"].ConnectionString);
+
+        private SqlCeDataAdapter dataAdapter_;
+        private DataTable logTable_;
+
         public MainWindow()
         {
             InitializeComponent();
+            Application.Current.Exit += ApplicationExit;
+
+            sqlConn_.Open();
+            dataAdapter_ = new SqlCeDataAdapter("select * from log", sqlConn_);
+            logTable_ = new DataTable("Log");
+            dataAdapter_.Fill(logTable_);
+
+            logDataGrid.ItemsSource = logTable_.DefaultView;
+        }
+
+        private void ApplicationExit(object sender, ExitEventArgs e)
+        {
+            sqlConn_.Close();
+        }
+
+        private void BtnCreateLogClick(object sender, RoutedEventArgs e)
+        {
+            log_.Info("Neueer LogEintrag!");
+          
+            //dataAdapter_= new SqlCeDataAdapter("select * from log", sqlConn_);
+            dataAdapter_.Fill(logTable_);
+
+            DataTableReader rd = new DataTableReader(logTable_);
+
+            logDataGrid.ItemsSource = rd;
+
+            listBox1.ItemsSource = new string[] {"sdffds", "sdfdsf", "sdfdsf"};
+
+            log_.Info(logTable_.Rows.Count);
+            log_.Info(logDataGrid.Items.Count);
         }
     }
 }
