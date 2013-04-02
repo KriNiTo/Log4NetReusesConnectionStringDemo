7abb8f667943ffadff10b41915408d5990e019d5
Christian Pfahl <cp@krinito.de>
2013-04-03 01:16:13 +0200

    Log4Net uses now the "primary" ConnectionString :-)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
+Shows how to use the standard app.config/web.config for configuring also Log4Net.
 
-TODO
+Benefit:
+Only one ConnectionString is easier to configure, to transform (deployment), to document 
+- especially if you have a solution with more than one Host (WebService-Host; Unit-Tests; ...)
+
+How ToDo:
+Step 1: Create class "CustomLog4NetAdoNetAppender.cs"
+Step 2: Change 2 entries in app.config (See <appender name="SQLCEAppender"...> and remarks)
+ 
+You can also diff to commit: "Classic Log4Net Config" (3a10bae1e6e5d7175580093ca2324d270bb427f4) to see the small changes.
---
+++ WpfAppWithLog4NetLogging/CustomLog4NetAdoNetAppender.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+
+using log4net.Appender;
+
+
+namespace Log4NetReusesConnectionStringDemo
+{
+    public class CustomLog4NetAdoNetAppender : AdoNetAppender
+    {
+        public new string ConnectionString
+        {
+            get
+            {
+                return base.ConnectionString;
+            }
+
+            set
+            {
+                // Getting ConnectionString with name defined in app.config:
+                //   /log4net/appender/connectionString/@value
+                base.ConnectionString = ConfigurationManager.ConnectionStrings[value].ConnectionString;
+            }
+        }
+    }
+}
